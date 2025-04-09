7c815ae9ef6aded4ef1d074fb634ab7dafb2a580
Ryan Araújo <167326341+araujocrk@users.noreply.github.com>
2025-04-09 17:31:11 -0300

    Update q3.py

--- lista02/q3.py
+++ lista02/q3.py
@@ -1,22 +1,31 @@
+import random
+
+def preencherListaFloat(qtd, lista):
+    for i in range(qtd):
+        lista.append(round(random.uniform(-100, 100), 2))
+    print('Lista preenchida com sucesso!')
+    print(f'Lista: {lista}')
+
 def inverterLista(lista):
     listaInvertida = []
     for i in lista:
         listaInvertida.insert(0, i)
-    return listaInvertida
+    return f'Lista invertida: {listaInvertida}'
 
 def main():
 # 3) Faça um programa que dada uma seqüência de n números, imprimi-la na ordem inversa à da
 # leitura.
+    lista = []
     while True:
         try:
-            qtdN = int(input('Digite a quantidade de números de uma sequência: '))
-            listaN = list(range(qtdN))
-            print(inverterLista(listaN))
+            qtd = int(input('Digite a quantidade números que deseja: '))
             break
             # IA
             # print(listaN[::-1])
         except:
             print('Quantidade de números inválida')
-    
+    preencherListaFloat(qtd, lista)
+    print(inverterLista(lista))
+
 if __name__ == '__main__':
     main()
