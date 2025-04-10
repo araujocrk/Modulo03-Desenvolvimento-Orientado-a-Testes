08c02889a4a3ac613e5476314a741b2279f79a52
Ryan Araújo <167326341+araujocrk@users.noreply.github.com>
2025-04-10 15:46:13 -0300

    Update q5.py

--- lista02/q5.py
+++ lista02/q5.py
@@ -1,31 +1,26 @@
+import random
+
+def preencherListaFloat(qtd, lista):
+    for i in range(qtd):
+        lista.append(round(random.uniform(-100, 100), 2))
+    print('Lista preenchida com sucesso!')
+    print(f'Lista: {lista}')
+
+def intercalarListas(l1, l2, l3):
+    for i in range(len(l1)):
+        l3.append(l1[i])
+        l3.append(l2[i])
+    print(f'Lista intercalada: {l3}')
+
 def main():
 # 5) Faça um programa que leia duas listas de 10 elementos numéricos cada um e intercale os
 # elementos deste em uma outra lista de 20 elementos.
     l1 = []
     l2 = []
     l3 = []
-    contador = 1
-    while contador <= 10:
-        try:
-            n = int(input(f'Digite o {contador} número da primeira lista: '))
-            l1.append(n)
-            contador += 1
-        except:
-            print(f'Número {contador} inválido. Tente novamente')
-            
-    contador = 1
-    while contador <= 10:
-        try:
-            n = int(input(f'Digite o {contador} número da segunda lista: '))
-            l2.append(n)
-            contador += 1
-        except:
-            print(f' número {contador} inválido. Tente novamente')
-            
-    for i in range(len(l1)):
-        l3.append(l1[i])
-        l3.append(l2[i])
-    print(l3)
-                    
+    preencherListaFloat(10, l1)
+    preencherListaFloat(10, l2)
+    intercalarListas(l1, l2, l3)
+       
 if __name__ == '__main__':
     main()
